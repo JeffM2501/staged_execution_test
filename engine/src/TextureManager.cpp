20f890089d546b3698385d13bffab899ed0e420d
Jeffery Myers <jeffm2501@gmail.com>
2026-02-19 08:31:28 -0800

    texture manager stub

---
+++ engine/src/TextureManager.cpp
@@ -0,0 +1,22 @@
+#include "TextureManager.h"
+
+#include <unordered_map>
+
+namespace TextureManager
+{
+
+    void Init()
+    {
+
+    }
+
+    void Shutdown()
+    {
+
+    }
+
+    TextureReference GetTexture(size_t hash)
+    {
+
+    }
+}
